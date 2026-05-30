4a9903155b819b68388a1fa46c5ef4a1a3aa46dc
Ricardo T <rtorre79@email.asu.edu>
2026-05-29 22:26:08 -0700

    4 / 6 challenges done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 > num2) {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
 
-};
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+
+    let total = 0;
+    for (let i = num1; i <= num2; i++)
+    {
+        total += i;
+    }
+    return total;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
